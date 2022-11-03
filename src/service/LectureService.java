d1a25650bb42507e5dc4cb62b4a2636e9bd9a2cd
AndriyT2 <22andriyt@gmail.com>
2022-11-03 20:34:13 +0200

    Problems :(

--- src/service/LectureService.java
+++ src/service/LectureService.java
@@ -1,4 +1,15 @@
 package service;
+import models.Lecture;
 
 public class LectureService {
+    public Lecture createLecture() {
+        return new Lecture();
+    }
+
+    public Lecture createLecture(int idCourse) {
+        this.idCourse = idCourse;
+        return new Lecture();
+
+    }
+
 }
