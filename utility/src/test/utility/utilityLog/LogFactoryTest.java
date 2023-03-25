e37b927b487385ad4637ba2fdf5bf74ea9297ad8
AndriyT2 <22andriyt@gmail.com>
2023-03-25 20:50:08 +0200

    Homework35/25.03.23

--- utility/src/test/utility/utilityLog/LogFactoryTest.java
+++ utility/src/test/utility/utilityLog/LogFactoryTest.java
@@ -1,42 +1,58 @@
 package utility.utilityLog;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import utility.utilityLog.Log;
-import utility.utilityLog.LogFactory;
-import utility.utilityLog.LogLevel;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class LogFactoryTest {
 
+    @DisplayName("errorLog")
     @Test
-    public void shouldReturnLogWhereLogLevelIsError() {
-        assertEquals(2, 3);
-
+    public void shouldCreateLogWhereLogLevelIsError() {
         Exception e = new RuntimeException();
 
-        Log log = new Log(this.getClass().getName(), LogLevel.ERROR, "message", e.getStackTrace());
-                //testing
-        Assertions.assertEquals(log , LogFactory.error(this.getClass().getName(), "message", e.getStackTrace()));
-
+        //testing
+        Assertions.assertNotEquals(LogLevel.INF0, LogFactory.error(this.getClass().getName(), "Log test", e.getStackTrace()).getLevel());
     }
 
     @Test
-    public  void warning() {
-        System.out.println("warning");
+    public void returnFalseIfLogLevelIsNotError() {
+        Exception e = new RuntimeException();
+        int count = LogFactory.getLogsStorage().size() + 1;
+
+        //testing
+        Assertions.assertEquals(LogLevel.ERROR, LogFactory.error(this.getClass().getName(), "Log test", e.getStackTrace()).getLevel());
+        assertEquals(count, LogFactory.getLogsStorage().size());
     }
 
+
+    @DisplayName("warningLog")
     @Test
-    public void info() {
-        Log log= new Log(this.getClass().getName(), LogLevel.INF0, "m");
-        assertEquals(log.toString(), LogFactory.info(this.getClass().getName(), "m").toString());
+    public void shouldCreateLogWhereLogLevelIsWarning() {
+        Exception e = new RuntimeException();
+        int count = LogFactory.getLogsStorage().size() + 1;
+        //testing
+        Assertions.assertEquals(LogLevel.WARNING, LogFactory.warning(this.getClass().getName(), "Log test", e.getStackTrace()).getLevel());
+
     }
 
+    @DisplayName("infoLog")
     @Test
-    public void debug() {
+    public void shouldCreateLogWhereLogLevelIsInfo() {
+        int count = LogFactory.getLogsStorage().size() + 1;
+        //testing
+        Assertions.assertEquals(LogLevel.INF0, LogFactory.info(this.getClass().getName(), "Log test").getLevel());
+    }
+
+    @DisplayName("debugLog")
+    @Test
+    public void shouldCreateLogWhereLogLevelIsDebug() {
+        int count = LogFactory.getLogsStorage().size() + 1;
+        //testing
+        Assertions.assertEquals(LogLevel.DEBUG, LogFactory.debug(this.getClass().getName(), "Log test").getLevel());
+
     }
 
 }
